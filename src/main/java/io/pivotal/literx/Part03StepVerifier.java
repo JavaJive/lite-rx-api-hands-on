e57d9dff48499ae7dc21263cb669d3b21beaccdc
Moa Thoren <moa.thoren@trustly.com>
2018-12-17 16:48:11 +0100

    implemented expectFooBarError and eexpectSkylerJesseComplete

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -39,24 +39,21 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux).expectNext("foo").expectNext("bar").expectError(RuntimeException.class);
 	}
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
-	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier.create(flux).expectNext(User.SKYLER).expectNext(User.JESSE).expectComplete();
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.create(flux).thenAwait().expectComplete();
 	}
 
 //========================================================================================
